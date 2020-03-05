21e7099f6f2d6c8803c67af3a3477487799f3a23
ahmed-behiry <61540211+ahmed-behiry@users.noreply.github.com>
2020-03-05 20:36:11 +1100

    Add files via upload

---
+++ thinking_of_a_number.py
@@ -0,0 +1,18 @@
+#thinking of a number 
+
+#Generate random number 
+import random
+num= random.randint(1,101)
+
+#Tell user to guess a number, anywhere between 1 – 100 
+guess = input("I'm thinking of a number between 1 and 100, guess the number"+"\n")
+
+#Compare input to random number 
+while not int(guess) == num:
+    if int(guess) > num:
+        guess= input("your guess is higher than the number, try again"+"\n")
+    else:
+        guess= input("your guess is lower than the number, try again"+"\n")
+
+print('congratulations!')
+
