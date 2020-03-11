8cd6a467972687be820d1349f2326937229e8726
ahmed-behiry <61540211+ahmed-behiry@users.noreply.github.com>
2020-03-12 09:56:28 +1100

    Update Rock_Paper_Scissors_no_IFs.py

--- Rock_Paper_Scissors_no_IFs.py
+++ Rock_Paper_Scissors_no_IFs.py
@@ -1,17 +1,24 @@
 import random
-
-# game words list
 game = ["Rock", "Paper", "Scissors"]
+result_dic = {"Rock_R": "it is draw!", "Rock_P": "you win!", "Rock_S": "you lose!",
+              "Paper_R": "you lose!", "Paper_P": "it is draw!", "Paper_S": "you win!",
+              "Scissors_R": "you win!", "Scissors_P": "you lose!", "Scissors_S": "it is draw!",
+              "Rock_r": "it is draw!", "Rock_p": "you win!", "Rock_s": "you lose!",
+              "Paper_r": "you lose!", "Paper_p": "it is draw!", "Paper_s": "you win!",
+              "Scissors_r": "you win!", "Scissors_p": "you lose!", "Scissors_s": "it is draw!"}
+again = "Y"
+while again in ["Y","y"]:
+    num = random.randint(0, 2)
+    comp_choice = game[num]
 
-# choose randomly between 0,1,2
-num = random.randint(0, 2)
+    user_choice = input(
+        "what is your move?" + "\n" + "Enter R for Rock" + "\n" + "Enter P for Paper" + "\n" + "Enter S for Scissors" + "\n")
 
-comp_choice = game[num]
-
-user_choice = input(
-    "what is your move?" + "\n" + "Enter R for Rock" + "\n" + "Enter P for Paper" + "\n" + "Enter S for Scissors" + "\n")
-
-result_dic = {"Rock_R": "it is draw!", "Rock_P": "you win!", "Rock_S": "you lose!", "Paper_R": "you lose!", "Paper_P": "it is draw!", "Paper_S": "you win!", "Scissors_R": "you win!", "Scissors_P": "you lose!", "Scissors_S": "it is draw!"}
-
-print("My choice was "+comp_choice)
-print(result_dic[comp_choice+"_"+user_choice])
+    while user_choice not in ["R","P","S","r","p","s"]:
+        user_choice = input(
+            "invalid choice" + "\n" + "Enter R for Rock" + "\n" + "Enter P for Paper" + "\n" + "Enter S for Scissors" + "\n")
+    print("My choice was "+comp_choice)
+    print(result_dic[comp_choice+"_"+user_choice])
+    again = input("do you want to play again?"+"\n" + "Enter Y for Yes" + "\n" + "Enter N for No"+ "\n")
+    while again not in ["Y","N","y","n"]:
+        again = input("invalid choice"+"\n" + "Enter Y for Yes" + "\n" + "Enter N for No"+ "\n")
